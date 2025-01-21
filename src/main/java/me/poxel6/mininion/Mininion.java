368de1c72bd5585d4f9acd6c3555fb9f8bcd2a16
PoXel6 <iampoxel6@gmail.com>
2025-01-21 14:38:41 +0330

    Batman

---
+++ src/main/java/me/poxel6/mininion/Mininion.java
@@ -0,0 +1,17 @@
+package me.poxel6.mininion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mininion extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
