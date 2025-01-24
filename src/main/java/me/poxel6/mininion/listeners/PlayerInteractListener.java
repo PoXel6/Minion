8990d4e32c516eff6a270daf8fe9aca24f49b59e
PoXel6 <iampoxel6@gmail.com>
2025-01-24 22:13:16 +0330

    feat(listener): made a listener which stops the player from interacting with the minion

--- src/main/java/me/poxel6/mininion/listeners/PlayerInteractListener.java
+++ src/main/java/me/poxel6/mininion/listeners/PlayerInteractListener.java
@@ -1,4 +1,20 @@
 package me.poxel6.mininion.listeners;
 
-public class PlayerInteractListener {
+import me.poxel6.mininion.minions.Minion;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
+
+public class PlayerInteractListener implements Listener {
+
+  @EventHandler
+  public void onPlayerInteractEvent(PlayerArmorStandManipulateEvent event) {
+    if (event.getRightClicked() instanceof Minion) {
+      var player = event.getPlayer();
+      if (player.isSneaking()) {
+        player.giveExp(1000);
+      }
+    }
+    event.setCancelled(true);
+  }
 }
