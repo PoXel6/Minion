b4fafe5b897e4f99a3e633222d3e9e537b9190c3
PoXel6 <iampoxel6@gmail.com>
2025-01-24 22:15:46 +0330

    feat(global): register the minion command

--- src/main/java/me/poxel6/mininion/Mininion.java
+++ src/main/java/me/poxel6/mininion/Mininion.java
@@ -1,17 +1,20 @@
 package me.poxel6.mininion;
 
+import me.poxel6.mininion.commands.MinionCommand;
+import me.poxel6.mininion.listeners.PlayerInteractListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Mininion extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+  @Override
+  public void onEnable() {
+    getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
+    Bukkit.getPluginCommand("minion").setExecutor(new MinionCommand());
+  }
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
 }
