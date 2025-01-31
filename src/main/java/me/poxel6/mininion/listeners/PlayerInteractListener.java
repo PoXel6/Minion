1285c0b040d4913714e26ad32345ae9ade34e33e
PoXel6 <iampoxel6@gmail.com>
2025-01-31 21:28:02 +0330

    feat(listener): listener is now able to destroy upon shit clicking and open menu on right click

--- src/main/java/me/poxel6/mininion/listeners/PlayerInteractListener.java
+++ src/main/java/me/poxel6/mininion/listeners/PlayerInteractListener.java
@@ -1,6 +1,6 @@
 package me.poxel6.mininion.listeners;
 
-import me.poxel6.mininion.minions.Minion;
+import me.poxel6.mininion.menus.MinionMainMenu;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
@@ -9,10 +9,12 @@ public class PlayerInteractListener implements Listener {
 
   @EventHandler
   public void onPlayerInteractEvent(PlayerArmorStandManipulateEvent event) {
-    if (event.getRightClicked() instanceof Minion) {
+    if (event.getRightClicked().hasMetadata("isMinion")) {
       var player = event.getPlayer();
       if (player.isSneaking()) {
-        player.giveExp(1000);
+        event.getRightClicked().setHealth(0);
+      } else {
+        new MinionMainMenu(player);
       }
     }
     event.setCancelled(true);
