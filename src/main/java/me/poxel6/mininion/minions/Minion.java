275ce5b9b17d60dc1422d77198cf20d010be2a9e
PoXel6 <iampoxel6@gmail.com>
2025-01-31 20:34:42 +0330

    feat(minion): minion is where i want it, just need refactoring

--- src/main/java/me/poxel6/mininion/minions/Minion.java
+++ src/main/java/me/poxel6/mininion/minions/Minion.java
@@ -1,32 +1,39 @@
 package me.poxel6.mininion.minions;
 
+import me.poxel6.mininion.Mininion;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class Minion implements ArmorStand {
+public abstract class Minion {
+  private static final String MINION_METADATA_KEY = "isMinion";
   @NotNull private final Player owner;
+  private final ArmorStand minion;
 
-  Minion(@NotNull Player owner) {
+  protected Minion(@NotNull Player owner) {
     this.owner = owner;
-
     var location = getPlayerLocation(owner);
-    var armorStand = (ArmorStand) location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
+    minion = (ArmorStand) location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
 
-    armorStand.setSmall(true);
-    armorStand.setGravity(false);
-    armorStand.setCustomName("Mininions");
-    armorStand.setInvulnerable(true);
-    armorStand.setArms(true);
+    minion.setCustomName("Mininion");
+    minion.setCustomNameVisible(true);
+    minion.setSmall(true);
+    minion.setGravity(false);
+    minion.setInvulnerable(true);
+    minion.setArms(true);
 
-    armorStand.getEquipment().setHelmet(new ItemStack(Material.PLAYER_HEAD));
-    armorStand.getEquipment().setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
-    armorStand.getEquipment().setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
-    armorStand.getEquipment().setBoots(new ItemStack(Material.LEATHER_BOOTS));
+    // get these from the config file or something
+    minion.getEquipment().setHelmet(new ItemStack(Material.PLAYER_HEAD));
+    minion.getEquipment().setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
+    minion.getEquipment().setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
+    minion.getEquipment().setBoots(new ItemStack(Material.LEATHER_BOOTS));
+
+    minion.setMetadata(MINION_METADATA_KEY, new FixedMetadataValue(Mininion.getInstance(), true));
   }
 
   private Location getPlayerLocation(@NotNull Player player) {
