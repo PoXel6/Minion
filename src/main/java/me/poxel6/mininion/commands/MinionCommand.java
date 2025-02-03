5bfada9398ece83b3b009593ab461f124e21df53
PoXel6 <iampoxel6@gmail.com>
2025-02-03 12:48:00 +0330

    feat(command): minion command is at working stage

--- src/main/java/me/poxel6/mininion/commands/MinionCommand.java
+++ src/main/java/me/poxel6/mininion/commands/MinionCommand.java
@@ -1,5 +1,6 @@
 package me.poxel6.mininion.commands;
 
+import me.poxel6.mininion.minions.StoneMinion;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,8 +10,9 @@ public class MinionCommand implements CommandExecutor {
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-      //      new Minion(player);
-      return sender instanceof Player player;
+    if (sender instanceof Player player) {
+      new StoneMinion(player);
+    }
+    return true;
   }
 }
