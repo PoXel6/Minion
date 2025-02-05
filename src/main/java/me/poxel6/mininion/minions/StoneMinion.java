958305b61c29da7b5b08fa439e7aff0409da5482
PoXel6 <iampoxel6@gmail.com>
2025-02-05 17:51:05 +0330

    feat(minion): made StoneMinion prototype.

---
+++ src/main/java/me/poxel6/mininion/minions/StoneMinion.java
@@ -0,0 +1,10 @@
+package me.poxel6.mininion.minions;
+
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class StoneMinion extends Minion {
+  public StoneMinion(@NotNull Player owner) {
+    super(owner);
+  }
+}
