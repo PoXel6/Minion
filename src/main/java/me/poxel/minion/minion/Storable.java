b3dac32483e238e3f2d608ad627a16aad56b0a0f
PoXel6 <iampoxel6@gmail.com>
2025-03-21 12:02:01 +0330

    refactor(minion): rename MinionStorage.java to Storable.java

--- src/main/java/me/poxel/minion/minion/Storable.java
+++ src/main/java/me/poxel/minion/minion/Storable.java
@@ -4,7 +4,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 
-public interface MinionStorage {
+public interface Storable {
 
 
 	Inventory getInventory();
