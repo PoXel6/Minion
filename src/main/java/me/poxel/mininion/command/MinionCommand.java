01666bdfc04d14004665a86f1c7e1ea6e90bbe09
PoXel6 <iampoxel6@gmail.com>
2025-03-21 15:38:35 +0330

    feat(command): made MinionCommand#getSender().

--- src/main/java/me/poxel/mininion/command/MinionCommand.java
+++ src/main/java/me/poxel/mininion/command/MinionCommand.java
@@ -3,15 +3,21 @@ package me.poxel.mininion.command;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Subcommand;
+import lombok.Getter;
 import me.poxel.mininion.Mininion;
 import me.poxel.mininion.config.ConfigManager;
 import me.poxel.mininion.minion.StoneMinion;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 
+@SuppressWarnings("unused")
 public class MinionCommand extends BaseCommand {
 
+
+	@Getter private static Player sender;
 
 	@SuppressWarnings("deprecation")
 	public MinionCommand() {
@@ -21,7 +27,9 @@ public class MinionCommand extends BaseCommand {
 	@Default
 	public void onCommand(CommandSender sender) {
 		if (sender instanceof Player owner) {
+			MinionCommand.sender = owner;
 			new StoneMinion(owner);
+			return;
 		}
 		sender.sendMessage("Only players can use this command!");
 	}
@@ -29,5 +37,6 @@ public class MinionCommand extends BaseCommand {
 	@Subcommand("reload")
 	public void onReload(CommandSender sender) {
 		ConfigManager.reload(Mininion.getInstance());
+		sender.sendMessage(Component.text("Reloaded Config!").color(NamedTextColor.GREEN));
 	}
 }
