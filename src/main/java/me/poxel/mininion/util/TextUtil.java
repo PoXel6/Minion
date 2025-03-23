151d94be7bd0d6056f24c473a7ef0c2fd81bb333
PoXel6 <iampoxel6@gmail.com>
2025-03-23 12:02:07 +0330

    feat(util): made TextUtil#miniMessageDeserializer() to serialize from MiniMessage#miniMessage().

--- src/main/java/me/poxel/mininion/util/TextUtil.java
+++ src/main/java/me/poxel/mininion/util/TextUtil.java
@@ -3,6 +3,7 @@ package me.poxel.mininion.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 
@@ -12,5 +13,9 @@ public abstract class TextUtil {
 
 	public static Component legacyDeserializer(String input) {
 		return LegacyComponentSerializer.legacyAmpersand().deserialize(input);
+	}
+
+	public static Component miniMessageDeserializer(String input) {
+		return MiniMessage.miniMessage().deserialize(input);
 	}
 }
