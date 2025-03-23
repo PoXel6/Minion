bbc70599c374178d5c8946a73a1dd6ed2b4c9d1e
PoXel6 <iampoxel6@gmail.com>
2025-03-23 11:40:40 +0330

    feat(minion): made minion ownable.

--- src/main/java/me/poxel/mininion/minion/Minion.java
+++ src/main/java/me/poxel/mininion/minion/Minion.java
@@ -2,6 +2,7 @@ package me.poxel.mininion.minion;
 
 import lombok.Getter;
 import me.poxel.mininion.Mininion;
+import me.poxel.mininion.minion.trait.Ownable;
 import me.poxel.mininion.minion.trait.Persistable;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.ArmorStand;
@@ -9,15 +10,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataType;
 
 
-public class Minion implements Persistable {
+public class Minion implements Persistable, Ownable {
 
 
 	@Getter private static final String MINION_KEY = "isMinion";
 	@Getter private static final Minion instance = MinionFactory.create();
 	private final Mininion PLUGIN = Mininion.getInstance();
 	private final ArmorStand armorStand = MinionFactory.getMinion();
+	private final Player owner;
 
 	protected Minion(Player owner) {
+		this.owner = owner;
 		armorStand.getPersistentDataContainer().set(getKey(), PersistentDataType.BOOLEAN, getValue());
 	}
 
@@ -31,6 +34,12 @@ public class Minion implements Persistable {
 	public Boolean getValue() {
 		return true;
 	}
+	//</editor-fold>
 
+	//<editor-fold desc="Ownable">
+	@Override
+	public Player getOwner() {
+		return owner;
+	}
 	//</editor-fold>
 }
--- src/main/java/me/poxel/mininion/minion/trait/Ownable.java
+++ src/main/java/me/poxel/mininion/minion/trait/Ownable.java
@@ -6,7 +6,5 @@ import org.bukkit.entity.Player;
 public interface Ownable {
 
 
-	void setOwner(Player owner);
-
-	void getOwner(Player owner);
+	Player getOwner();
 }
